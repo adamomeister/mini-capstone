c76fea2749d79e8a9a1bf8dd40c0b5e5feef7cb7
CHRIS ADAMO <adamomeister@gmail.com>
2018-10-22 20:23:00 -0500

    remove supplier id from Suppliers, add supplier_id to Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_005015) do
+ActiveRecord::Schema.define(version: 2018_10_23_010113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_005015) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
@@ -30,7 +31,6 @@ ActiveRecord::Schema.define(version: 2018_10_23_005015) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "supplier_id"
   end
 
 end
